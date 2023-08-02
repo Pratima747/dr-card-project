44e55568659ec9cab944d2a2bb09ee915f19c06c
digitalready-nikki <140424981+digitalready-nikki@users.noreply.github.com>
2023-08-02 10:58:03 -0400

    Add files via upload
    8/2/23

--- blackjack.py
+++ blackjack.py
@@ -3,22 +3,41 @@
 
 # Import statements
 from card import Card
-
+game_over= False
 # Blackjack
 def black_jack():
     deck = Card.new_deck()
-    mycard= [deck.pop(), deck.pop()]
-    player_card= mycard
+    player_hand= [deck.pop(), deck.pop()]
+    dealer_hand= [deck.pop(), deck.pop()]
+     
 # Ask user if they want to add a card
     Add_card =input("yes or no")
     # if Add_card =="yes":
     #     mycard= player_card .appenddeck.pop()
+    
 
     while Add_card =="yes":
-        player_card.append(deck.pop())
+        player_hand.append(deck.pop())
+        print(player_hand)
         Add_card =input("yes or no")
- 
- 
+
+   
+
+    if player_hand > dealer_hand:
+        if player_hand <= 21:
+            print("You won")
+        else: 
+            print("You lost")
+    if player_hand< dealer_hand:
+        if dealer_hand <= 21:
+            print("You lost")
+        else:
+            print("You won")
+    if  player_hand== dealer_hand:
+       print("Tie")
+
+ # check who won or lost 
+
 
 # Code that runs when script is called from terminal
 # ex: python my_card_game.py
